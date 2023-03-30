cfd3d84c9819f68b44124f282fc6f05500cbb141
AnaFern2023 <AnaFern2023@gmail.com>
2023-03-30 13:38:50 +0200

    new commit 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const div = document.querySelector("div");
 
 fetch(
-  "https://api.openweathermap.org/data/2.5/weather?lat=	51.509865&lon=-0.118092&appid=97274ed796341a5e8aaa7d2bd6602b2f"
+  "https://api.openweathermap.org/data/2.5/weather?lat=	51.509865&lon=-0.118092&appid=b92d874f14985184b6a96ed8d4fc539b"
 )
   .then((response) => response.json())
   .then((data) => console.log(data));

b9ba0031d0754bae2edf15bea77b6eab7c67e95a
Rizvan555 <rizvan84@gmx.de>
2023-03-30 13:28:44 +0200

    started header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
-const div = document.querySelector("div");
-
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?lat=	51.509865&lon=-0.118092&appid=97274ed796341a5e8aaa7d2bd6602b2f"
 )
   .then((response) => response.json())
   .then((data) => console.log(data));
-

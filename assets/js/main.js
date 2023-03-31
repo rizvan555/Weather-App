2b5957fa5425572935b83c0e5e944875d09e1a16
Rizvan555 <rizvan84@gmx.de>
2023-03-31 09:15:24 +0200

    project 90% finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,30 +6,61 @@ const humidity = document.querySelector(".humidity");
 const sunrise = document.querySelector(".sunrise");
 const sunset = document.querySelector(".sunset");
 const geo = document.querySelector(".geo");
+const headerTemperature = document.querySelector(".header-temperature");
+const headerCloudiness = document.querySelector(".header-cloudiness");
+const headerTime = document.querySelector(".header-time");
 
 const date = new Date();
-localTime.textContent = date.toLocaleString();
-
-const sunriseTimestamp = 1680154869;
-const sunriseDate = new Date(sunriseTimestamp * 1000);
-const sunriseTimeString = sunriseDate.toLocaleTimeString();
-
-const sunsetTimestamp = 1680200921;
-const sunsetDate = new Date(sunsetTimestamp * 1000);
-const sunsetTimeString = sunsetDate.toLocaleTimeString();
+const hour = date.getHours();
+const minute = date.getMinutes();
+const day = date.getDate();
+const monthNum = date.getMonth();
+const year = date.getFullYear();
+const month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const monthName = month[monthNum];
 
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?lat=	51.509865&lon=-0.118092&appid=b92d874f14985184b6a96ed8d4fc539b"
 )
   .then((response) => response.json())
   .then((data) => {
+    const tempInKelvin = `${data.main.temp}`;
+    const tempInCelsius = (tempInKelvin - 273.15).toFixed(1);
+
+    const sunriseTimestamp = `${data.sys.sunrise}`;
+    const sunriseDate = new Date(sunriseTimestamp * 1000);
+    const sunriseTimeString = sunriseDate.toLocaleTimeString();
+
+    const sunsetTimestamp = `${data.sys.sunset}`;
+    const sunsetDate = new Date(sunsetTimestamp * 1000);
+    const sunsetTimeString = sunsetDate.toLocaleTimeString();
+
+    localTime.textContent = `${hour}:${minute}, ${day} ${monthName} ${year}`;
     wind.textContent = `Fresh Breeze ${data.wind.speed} m/s West (${data.wind.deg})`;
     cloudniss.textContent = `${data.weather[0].description}`;
     pressure.textContent = `${data.main.pressure}`;
     humidity.textContent = `${data.main.humidity} %`;
     sunrise.textContent = `${sunriseTimeString} `;
     sunset.textContent = `${sunsetTimeString}`;
-    geo.textContent=`[${data.coord.lat}, ${data.coord.lon}]`
+    geo.textContent = `[${data.coord.lat} ${data.coord.lon}]`;
+    headerTemperature.textContent = `${tempInCelsius} °C`;
+    headerCloudiness.textContent = `${data.weather[0].description}`;
+    headerTime.textContent = `${
+      hour + 1
+    }:${minute}, ${day} ${monthName} ${year}`;
 
     console.log(data);
   });

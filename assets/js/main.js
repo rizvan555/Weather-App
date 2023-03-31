d0dafe0e407154740a4250680b6afd49e9da4bd7
“ClaudioSUPERCODE” <claudiosupercode@gmail.com>
2023-03-31 09:54:04 +0200

    windrichtung scleife gebaut

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ const month = [
 const monthName = month[monthNum];
 
 fetch(
-  "https://api.openweathermap.org/data/2.5/weather?lat=	51.509865&lon=-0.118092&appid=b92d874f14985184b6a96ed8d4fc539b"
+  "https://api.openweathermap.org/data/2.5/weather?lat=51.509865&lon=-0.118092&appid=b92d874f14985184b6a96ed8d4fc539b"
 )
   .then((response) => response.json())
   .then((data) => {
@@ -48,19 +48,36 @@ fetch(
     const sunsetDate = new Date(sunsetTimestamp * 1000);
     const sunsetTimeString = sunsetDate.toLocaleTimeString();
 
+    let windDir
+    if (data.wind.deg >= 45 && data.wind.deg <= 135) {
+      windDir = "East"
+    } else if (data.wind.deg > 135 && data.wind.deg <= 225) {
+      windDir = "South"
+    } else if (data.wind.deg > 225 && data.wind.deg <= 315) {
+      windDir = "West"
+    }
+    else {
+      windDir = "Nord"
+    }
+
+    function capitalize(string) {
+      return string.slice(0, 1).toUpperCase() + string.slice(1)
+    }
+
     localTime.textContent = `${hour}:${minute}, ${day} ${monthName} ${year}`;
-    wind.textContent = `Fresh Breeze ${data.wind.speed} m/s West (${data.wind.deg})`;
-    cloudniss.textContent = `${data.weather[0].description}`;
+    wind.textContent = `Fresh Breeze ${data.wind.speed} m/s ${windDir} (${data.wind.deg})`;
+    cloudniss.textContent = capitalize(data.weather[0].description);
     pressure.textContent = `${data.main.pressure}`;
     humidity.textContent = `${data.main.humidity} %`;
     sunrise.textContent = `${sunriseTimeString} `;
     sunset.textContent = `${sunsetTimeString}`;
-    geo.textContent = `[${data.coord.lat} ${data.coord.lon}]`;
+    geo.textContent = `[${data.coord.lat.toFixed(2)} ${data.coord.lon.toFixed(2)}]`;
     headerTemperature.textContent = `${tempInCelsius} °C`;
-    headerCloudiness.textContent = `${data.weather[0].description}`;
-    headerTime.textContent = `${
-      hour + 1
-    }:${minute}, ${day} ${monthName} ${year}`;
+    headerCloudiness.textContent = capitalize(data.weather[0].description);
+    headerTime.textContent = `${hour + 1
+      }:${minute}, ${day} ${monthName} ${year}`;
 
     console.log(data);
   });
+
+
